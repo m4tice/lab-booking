87589093fe9faf2f80c27b4e8a1c6014a4c8f121
m4tice <44093611+SledgyNguyen@users.noreply.github.com>
2023-05-03 11:27:45 +0700

    Integrate page.html

--- app/main.py
+++ app/main.py
@@ -1,29 +1,42 @@
 """Main"""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
+
+# =========================================================
+#### INITIALIZATION #######################################
+# =========================================================
 
 # app creation
 app = FastAPI()
 
+# templates
+TEMPLATE_DIRECTORY = "./templates"
+templates = Jinja2Templates(directory=TEMPLATE_DIRECTORY)
+
+
+# =========================================================
+#### VIEWS CREATION #######################################
+# =========================================================
 
 # Example URL: http://127.0.0.1:8000/
-@app.get("/")
-async def home():
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
     """
     Home page
     """
     data = {
-        "text": "Hello World"
+        "page": "Home Page"
     }
 
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
 # Example URL: http://127.0.0.1:8000/page/m4tice
-@app.get("/page/{page_name}")
-async def page(page_name: str):
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
     """
     Specific page
     """
@@ -31,7 +44,7 @@ async def page(page_name: str):
         "page": page_name
     }
 
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
 
 def main():
